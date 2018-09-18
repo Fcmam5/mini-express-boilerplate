3ce8d2c18ff3c37e790dff4bb08e1530f8087095
Fcmam5 <dr.fcmam5@gmail.com>
2018-09-18 23:32:35 +0100

    update decricated packages

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 
 // Database and passport setup
 // mongoose.Promise = global.Promise; //For hiding the "deprecated" warning
-mongoose.connect(configDB.url, {useMongoClient: true},function(err, next) {
+mongoose.connect(configDB.url, { useNewUrlParser: true }, function (err, next) {
   if (err) {
     console.error("Faild to load DB");
   } else {
@@ -40,7 +40,7 @@ mongoose.connect(configDB.url, {useMongoClient: true},function(err, next) {
 });
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
+mongoose.connection.on('error', function (err) {
   console.log('Mongoose connection Warning: error: ' + err);
 });
 
@@ -54,7 +54,7 @@ app.use(session({
   name: 'itismysessions',
   resave: false,
   saveUninitialized: false,
-  store: new mongoStore({mongooseConnection: mongoose.connection})
+  store: new mongoStore({ mongooseConnection: mongoose.connection })
 }));
 
 // Passport configuration
@@ -64,7 +64,7 @@ app.use(passport.session());
 app.use(flash());
 
 // To enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -92,9 +92,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 app.use(csrf());
-app.use(function(req, res, next){
- res.locals.csrftoken = req.csrfToken();
- next();
+app.use(function (req, res, next) {
+  res.locals.csrftoken = req.csrfToken();
+  next();
 });
 
 app.use('/', index);
@@ -102,14 +102,14 @@ app.use('/users', users);
 app.use('/api/users', userAPI);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
